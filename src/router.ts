a98f209934a8e6e2cef87fd19736c1203ef19d82
Ninglo <jiujianian@gmail.com>
2024-07-14 13:24:18 +0800

    feat: key practice page

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,8 @@ import RandomMode from "./pages/RandomMode.vue";
 import LeadMode from "./pages/LeadMode.vue";
 import FollowMode from "./pages/FollowMode.vue";
 import ParagraphMode from "./pages/PragraphMode.vue";
+import KeyPositionPracticeMode from "./pages/KeyPositionPracticeMode.vue";
+import KeyPositionPracticeModeLead from "./pages/KeyPositionPracticeModeLead.vue";
 import Settings from "./pages/Settings.vue";
 
 import { RouteRecordRaw } from "vue-router";
@@ -26,6 +28,16 @@ export const routes: RouteRecordRaw[] = [
     path: "/p-mode",
     name: "长句模式",
     component: ParagraphMode,
+  },
+  {
+    path: "/key-practice-mode",
+    name: "韵母练习",
+    component: KeyPositionPracticeMode,
+  },
+  {
+    path: "/key-practice-mode-lead",
+    name: "声母练习",
+    component: KeyPositionPracticeModeLead,
   },
   {
     path: "/settings",
